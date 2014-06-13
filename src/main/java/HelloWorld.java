cb6a97a3d60aaacbef3a8fb5725573801c0be39e
Viktor Chmel <viktor.chmel@gmail.com>
2014-06-13 21:17:53 +0300

    First commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 13.06.14
+ * Time: 20:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("dddddddddddddddddd");
+    }
+}

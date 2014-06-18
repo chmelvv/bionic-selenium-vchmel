dacf48ccf8421ea0403bb7f421090be16d6fdee5
Viktor Chmel <Viktor.Chmel@travisperkins.co.uk>
2014-06-18 19:11:04 +0300

    Lesson 3 finished 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,6 +5,7 @@
  * Time: 20:56
  * To change this template use File | Settings | File Templates.
  */
+//@.test
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello!");
